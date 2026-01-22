363101121381fd00cf18ce10266526605ed738ae
swfs0417 <axizp@snu.ac.kr>
2026-01-22 13:18:39 +0900

    feat: docstring 추가, update 추가

--- asset_management/app/statistics/router.py
+++ asset_management/app/statistics/router.py
@@ -10,6 +10,13 @@ router = APIRouter(prefix="/statistics")
 
 @router.get("/{asset_id}")
 def get_statistics(asset_id: int, stat_service: Annotated[StatisticsService, Depends()]) -> AssetStatistics:
+  '''통계를 불러옵니다.'''
   statistics = stat_service.get_statistics_for_asset(asset_id)
 
+  return statistics
+
+@router.get("/{asset_id}/update")
+def update_statistics(asset_id: int, stat_service: Annotated[StatisticsService, Depends()]) -> AssetStatistics:
+  '''통계를 갱신합니다.'''
+  statistics = stat_service.update_statistics_for_asset(asset_id)
   return statistics
